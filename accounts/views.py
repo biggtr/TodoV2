bcd6409968efec27cba8892595f52c43ad10f88c
bigtorresg <bigtorresg@gmail.com>
2023-08-30 18:51:18 +0300

    static files Configured

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,17 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.contrib.auth import login
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
 from accounts.models import CustomUser
 from .forms import CustomUserCreationForm
+from django.http import HttpResponseForbidden
 
 # Create your views here.
 
 
 def SignupView(request):
+    if request.user.is_authenticated:
+        return HttpResponseForbidden("You Already have an account")
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
